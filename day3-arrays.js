a1a8ffa0f6a4bbbf741376aa04cd6508843000e4
Mama S Ceesay <mamaceesy344@gmail.com>
2026-09-06 21:08:24 +0000

    feat: complete Day 3 arrays with push and length methods

--- day3-arrays.js
+++ day3-arrays.js
@@ -6,5 +6,18 @@ console.log(studentNames[1]); // Output: Mary
 studentNames.push("Ousainou");
 console.log(studentNames.length); // Output: 5
 
-const backendTools = ["javascript", "Git", "Node", "Supabase",];
+const backendTool = ["javascript", "Git", "Node", "Supabase",];
 console.log(backendTools[0]); // Output: javascript
+
+const backendTools = ["JavaScript", "Git", "Node"];
+
+// Adding a new tool to the end of the array
+backendTools.push("Supabase");
+
+// Printing the updated array
+console.log(backendTools); 
+// Output: [ 'JavaScript', 'Git', 'Node', 'Supabase' ]
+
+// Printing the total number of items
+console.log(backendTools.length); 
+// Output: 4
