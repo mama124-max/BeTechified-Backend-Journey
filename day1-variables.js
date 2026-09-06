62802467366c46bdd975f19bb6c07629cf058096
Mama S Ceesay <mamaceesy344@gmail.com>
2026-09-06 18:47:17 +0000

    feat: complete Day 1 variables and data types

---
+++ day1-variables.js
@@ -0,0 +1,6 @@
+const studentName = "Mama";
+const matriculationNumber = 22426047;
+let currentWeek = 1;
+
+console.log(`Student: ${studentName}, Matric: ${matriculationNumber}`);
+console.log(`Currently on Week ${currentWeek}`);
