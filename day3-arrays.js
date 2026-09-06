38ea09f844286231e215ff382fc1e5d2735d95fa
Mama S Ceesay <mamaceesy344@gmail.com>
2026-09-06 21:01:58 +0000

    feat: complete Day 3 arrays, indexing, and list practice

---
+++ day3-arrays.js
@@ -0,0 +1,10 @@
+const studentNames = ["Mama", "Mary", "Modou", "Burama"];
+
+console.log(studentNames[0]); // Output: Mama
+console.log(studentNames[1]); // Output: Mary
+
+studentNames.push("Ousainou");
+console.log(studentNames.length); // Output: 5
+
+const backendTools = ["javascript", "Git", "Node", "Supabase",];
+console.log(backendTools[0]); // Output: javascript
