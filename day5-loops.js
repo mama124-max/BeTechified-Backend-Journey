946f4e8c02af82b314d335f6c79d3e17fe6528dd
Mama S Ceesay <mamaceesy344@gmail.com>
2026-09-07 22:10:10 +0000

    feat: complete Day 5 loops practice

---
+++ day5-loops.js
@@ -0,0 +1,18 @@
+const backendTools = ["JavaScript", "Git", "Node", "Supabase"];
+
+// A standard for loop
+for (let i = 0; i < backendTools.length; i++) {
+    console.log("Current tool: " + backendTools[i]);
+}
+
+
+
+
+
+
+
+const serverTools = ["Express", "Koa", "Hapi", "Fastify", "NetJs"];
+
+for (let i = 0; i < serverTools.length; i++) {
+    console.log("Server tools: " + serverTools[i]);
+}
